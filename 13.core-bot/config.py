622fdf4ebd99d5dfd3ab4a615396746b93e62b35
louiskuo0983 <84321676+louiskuo0983@users.noreply.github.com>
2021-05-17 19:29:18 +0800

    Add files via upload

---
+++ 13.core-bot/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "8a791f9a-a759-4ac2-be37-ab7fb1afa3f6")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "627e34fabce540be9ecac13cf9fdcfe7")
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westus.api.cognitive.microsoft.com/")
